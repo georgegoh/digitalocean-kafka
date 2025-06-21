46f7ed7c7ddcb66d79ebf4e17edaf4509a23e0f7
George Goh <georgegohkokleong@gmail.com>
2025-06-22 00:15:40 +0800

    Colorize the failback script output when something significant is changed.

--- scripts/failback.sh
+++ scripts/failback.sh
@@ -5,7 +5,7 @@ for tag in `curl -s http://169.254.169.254/metadata/v1/tags`
 do
 	if [ "$tag" = 'failback' ]
 	then
-		echo "Failback tag detected. Performing failback actions."
+		echo -ne "\e[1;30;41m Failback tag detected. \e[0;37;40m \e[1;31;40mPerforming failback actions.\e[0;37;40m"
 		/usr/bin/systemctl stop confluent-zookeeper.service
 		/usr/bin/systemctl mask confluent-zookeeper.service
 		exit
